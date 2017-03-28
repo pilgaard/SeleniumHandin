3d774a33db5bd978959a8fbcf6e607d2c0757a45
pilgaard <emil.pilgaard@gmail.com>
2017-03-28 22:04:36 +0200

    finish

--- src/test/java/test/CarsTest1.java
+++ src/test/java/test/CarsTest1.java
@@ -4,6 +4,7 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.AfterClass;
 import org.junit.Assert;
+import static org.junit.Assert.assertThat;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -49,7 +50,7 @@ public class CarsTest1 {
         (new WebDriverWait(driver, WAIT_MAX)).until((ExpectedCondition<Boolean>) (WebDriver d) -> {
             WebElement e = d.findElement(By.tagName("tbody"));
             List<WebElement> rows = e.findElements(By.tagName("tr"));
-            Assert.assertThat(rows.size(), is(5));
+            assertThat(rows.size(), is(5));
             return true;
         });
     }
@@ -62,9 +63,10 @@ public class CarsTest1 {
         element.sendKeys("2002");
         element = driver.findElement(By.tagName("tbody"));
         List<WebElement> rows = element.findElements(By.tagName("tr"));
-        Assert.assertThat(rows.size(), is(2));        
+        assertThat(rows.size(), is(2));
     }
-     @Test
+
+    @Test
     //Verify the reverse filter functionality 
     public void test3() throws Exception {
         WebElement element = driver.findElement(By.id("filter"));
@@ -72,6 +74,69 @@ public class CarsTest1 {
         element.sendKeys(Keys.BACK_SPACE);
         element = driver.findElement(By.tagName("tbody"));
         List<WebElement> rows = element.findElements(By.tagName("tr"));
-        Assert.assertThat(rows.size(), is(5));
+        assertThat(rows.size(), is(5));
+    }
+
+    @Test
+    //Verify the sorting button functionality
+    public void test4() throws Exception {
+        WebElement element = driver.findElement(By.id("h_year"));
+        element.click();
+        element = driver.findElement(By.tagName("tbody"));
+        List<WebElement> rows = element.findElements(By.tagName("tr"));
+        WebElement row = rows.get(0);
+        assertThat(row.findElement(By.tagName("td")).getText(), is("938"));
+        row = rows.get((rows.size() - 1));
+        assertThat(row.findElement(By.tagName("td")).getText(), is("940"));
+    }
+    
+    @Test
+    //Verify the sorting button functionality
+    public void test5() throws Exception {
+        WebElement element = driver.findElement(By.tagName("tbody"));
+        List<WebElement> rows = element.findElements(By.tagName("tr"));
+        WebElement row = rows.get(0);
+        WebElement edit = row.findElement(By.tagName("a"));
+        edit.click();
+        element = driver.findElement(By.id("description"));
+        element.clear();
+        element.sendKeys("Cool car");
+        element = driver.findElement(By.id("save"));
+        element.click();
+        List<WebElement> colums = row.findElements(By.tagName("td"));
+        WebElement field = colums.get(5);
+        assertThat(field.getText(), is("Cool car"));
+    }
+    
+    @Test
+    //Verify the sorting button functionality
+    public void test6() throws Exception {
+        WebElement element = driver.findElement(By.id("new"));
+        element.click();
+        element = driver.findElement(By.id("save"));
+        element.click();
+        element = driver.findElement(By.id("submiterr"));
+        assertThat(element.getText(), is("All fields are required"));
+    }
+    
+    @Test
+    //Verify the sorting button functionality
+    public void test7() throws Exception {
+        WebElement element = driver.findElement(By.id("new"));
+        element.click();
+
+        driver.findElement(By.id("year")).sendKeys("2008");
+        driver.findElement(By.id("registered")).sendKeys("2002-05-05");
+        driver.findElement(By.id("make")).sendKeys("Kia");
+        driver.findElement(By.id("model")).sendKeys("Rio");
+        driver.findElement(By.id("description")).sendKeys("As new");
+        driver.findElement(By.id("price")).sendKeys("31000");
+
+        element = driver.findElement(By.id("save"));
+        element.click();
+
+        element = driver.findElement(By.tagName("tbody"));
+        List<WebElement> rows = element.findElements(By.tagName("tr"));
+        Assert.assertThat(rows.size(), is(6));
     }
 }
